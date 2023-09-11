1c6c92e33dd2361a1280bfe53aa101eabcbe0306
yannakode <yanann1997@gmail.com>
2023-09-11 20:41:22 -0300

    first commit

---
+++ src/test/java/com/carstore/carstore/CarstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carstore.carstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
